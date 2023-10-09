51b2c95dbd055c2972908aab53ab5ac195e00335
Charles Ching <42172631+win4r@users.noreply.github.com>
2023-10-09 22:20:49 +0800

    main

---
+++ Android-version/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AIMeta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Android-version/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AIMeta
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
